8b4c2fab8b7bf0bc550512d4d7f2381241615cb9
PAXANDDOS <40315177+PAXANDDOS@users.noreply.github.com>
2021-04-17 17:22:35 +0300

    Update socket.js

--- assets/js/socket.js
+++ assets/js/socket.js
@@ -10,7 +10,7 @@ window.addEventListener('DOMContentLoaded', function () {
     if(window.location.href.indexOf("board") > -1) {
         // новое соединение открываем, если старое соединение закрыто
         if (socket === undefined || socket.readyState !== 1) {
-            socket = new WebSocket("ws://localhost:5656");
+            socket = new WebSocket("wss://93.75.70.113:5656");
         } else {
             showMessage('Надо закрыть уже имеющееся соединение');
         }
