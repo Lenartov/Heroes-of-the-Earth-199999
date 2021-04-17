8a12b5134326c1edba7d76e490ca804691c92e44
PAXANDDOS <40315177+PAXANDDOS@users.noreply.github.com>
2021-04-17 13:05:18 +0300

    please work

--- server/GameSession.php
+++ server/GameSession.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once "GameClasses.php";
-include_once '/application/controllers/controller_database.php';
+require_once "application/controllers/controller_database.php";
 
 class GameSession
 {
